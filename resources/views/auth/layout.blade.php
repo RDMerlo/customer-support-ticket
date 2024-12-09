1e8b5590da48b1a58846ce424a16b4b074fc0856
Dim Sultanov <dim-sultanov@outlook.com>
2024-12-09 20:06:49 +0500

    Правки title шаблона авторизации

--- resources/views/auth/layout.blade.php
+++ resources/views/auth/layout.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>Активное долголетие</title>
+    <title>M.Support — Авторизация</title>
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="icon" href="{{asset('favicon.ico')}}" type="image/x-icon">
     <link type="image/x-icon" href="{{asset('favicon.ico')}}" rel="shortcut icon">
