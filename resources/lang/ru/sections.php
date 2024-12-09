7810f3fc47ec7557cb85b4f2f153b2276c6a60e1
Dim Sultanov <dim-sultanov@outlook.com>
2024-12-09 16:12:21 +0500

    Добавил файлы локализации

---
+++ resources/lang/ru/sections.php
@@ -0,0 +1,33 @@
+<?php
+
+return [
+    'about' => 'О проекте',
+    'feedback' => 'Обратная связь',
+    'country' => 'Страны',
+    'city' => 'Города',
+    'map' => 'Карта',
+    'calendar_event' => 'Публичные мероприятия',
+    'past_event' => 'Прошедшие мероприятия',
+    'expert' => 'Эксперты',
+    'category_knowledge_base' => 'Категория базы знаний',
+    'type_research' => 'Тип исследования',
+    'knowledge_base' => 'Материалы базы знаний',
+    'best_practice' => 'Лучшие практики',
+    'soc_panel' => 'Социологическая панель',
+    'news' => 'Новости',
+    'chapters' => 'Разделы лендинга',
+    'speakers' => 'Спикеры',
+    'event_moderators' => 'Модераторы',
+    'program' => 'Программа',
+    'users' => 'Пользователи',
+    'chart' => 'График лояльности',
+    'contacts' => 'Контакты',
+    'subscriber' => 'Подписчики',
+    'landing_page' => 'Лендинг',
+    'materials' => 'Материалы',
+    'video_materials' => 'Видео материалы',
+    'category_record' => 'Категория заявок',
+    'processing_statuses' => 'Процесс заявок',
+    'support_ticket_record' => 'Заявки',
+
+];
