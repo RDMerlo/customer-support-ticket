af1c48f302a637b4ad8338f662f3b88f1925f561
Dim Sultanov <dim-sultanov@outlook.com>
2024-12-09 19:13:24 +0500

    Добавление фавикона

--- resources/views/admin/layouts/index.blade.php
+++ resources/views/admin/layouts/index.blade.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Admin</title>
+    <link rel="icon" href="{{asset('favicon.ico')}}" type="image/x-icon">
     <link rel="stylesheet" href="{{ asset('css/admin/app.css') }}"/>
 {{--    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">--}}
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
--- resources/views/public/layouts/index.blade.php
+++ resources/views/public/layouts/index.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>M.Support</title>
+    <link rel="icon" href="{{asset('favicon.ico')}}" type="image/x-icon">
     <link type="image/x-icon" href="{{asset('favicon.ico')}}" rel="shortcut icon">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link href="{{asset('css/public/theme.css')}}?v1.0" rel="stylesheet">
