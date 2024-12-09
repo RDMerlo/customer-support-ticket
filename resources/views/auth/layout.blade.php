f676dadb345292794c10c4261599274f670e7aab
Dim Sultanov <dim-sultanov@outlook.com>
2024-12-09 20:05:12 +0500

    Добавление библиотек бутстрапа и jquery на страницу авторизации

--- resources/views/auth/layout.blade.php
+++ resources/views/auth/layout.blade.php
@@ -5,6 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>Активное долголетие</title>
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="icon" href="{{asset('favicon.ico')}}" type="image/x-icon">
+    <link type="image/x-icon" href="{{asset('favicon.ico')}}" rel="shortcut icon">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link href="{{ asset('assets/css/app.css') }}" rel="stylesheet">
     @stack('styles')
 </head>
@@ -12,6 +15,8 @@
 <main>
     @yield('content')
 </main>
+<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 <script src="{{ asset('js/admin/app.js') }}"></script>
 @stack('scripts')
 </body>
